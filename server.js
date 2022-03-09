7e08c1f2270e76b4b13bfe05b95859637b087edb
OzKr Mebasser <82064221+OzkrMebasser@users.noreply.github.com>
2022-03-08 22:07:10 -0500

    commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const { app } = require('./app');
+
+// Utils
+const { sequelize } = require('./util/database');
+const { initModels } = require('./util/initModels');
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Database authenticated'))
+  .catch((err) => console.log(err));
+
+// Models relations
+initModels();
+
+sequelize
+  .sync()
+  .then(() => console.log('Database synced'))
+  .catch((err) => console.log(err));
+
+
+const startServer = () =>{
+  const PORT = process.env.PORT || 4000;
+};
+
+app.listen(PORT, () => {
+  console.log(`Express app running in ${PORT}`);
+});
